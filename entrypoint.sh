b2713d4fea475538333e793193fa87170b60ff17
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-28 12:51:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # make start
 if [ ! -f "/etc/influxdb/influxdb.conf" ]; then cp /influxdb.conf /etc/influxdb/ ;fi
 
+# ssh
+if [ -f "/runssh.sh" ]; then /runssh.sh; fi
+
 if [ "${1:0:1}" = '-' ]; then
     set -- influxd "$@"
 fi
